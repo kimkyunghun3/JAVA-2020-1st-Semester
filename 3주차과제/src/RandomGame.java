b02f27725018ba4ae398d69e162c36733efdc89c
kimkyunghun3 <kimkyunghun3@naver.com>
2020-03-31 15:05:04 +0900

    RandomGame program

---
+++ 3주차과제/src/RandomGame.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RandomGame {
+    public static void main(String args[]) {
+        System.out.println("Guess a magic number between 0 and 100");
+        Scanner scanner = new Scanner(System.in);
+        double randomValue = Math.random();
+
+        int intValue = (int) (randomValue * 100) + 1;
+        while (true) {
+            System.out.println("Enter your guess:");
+            int guess = scanner.nextInt();
+            if (guess == intValue) {
+                System.out.println("yes, the number is " + guess);
+                break;
+            } else if (guess > intValue) {
+                System.out.println("your guess it too high");
+            } else if (guess < intValue) {
+                System.out.println("your guess is too low");
+            }
+        }
+    }
+}
