6faede8af1dfecfa301d34047ae4ac709cfc521a
kimkyunghun3 <kimkyunghun3@naver.com>
2020-04-07 00:05:50 +0900

    첫 번째 레포트 주석에 설명 수정

--- 첫번째레포트/src/FindPrimes.java
+++ 첫번째레포트/src/FindPrimes.java
@@ -2,7 +2,7 @@
  * 파일명 : FindPrimes.java
  * 입력 : -
  * 출력 : 각 줄의 10개씩 소수 50개 출력
- * 작성일 : 2020년 4월 일
+ * 작성일 : 2020년 4월 7일
  * 작성자 : 20176088 김경훈
  *
  * Q. 각 줄에 10개씩 소수 50개를 출력
@@ -12,8 +12,8 @@ public class FindPrimes
 {
     public static boolean isPrime(int x)
     {
-        for (int i = 2; i <= Math.sqrt(x); i++) {
-            if (x % i == 0) {
+        for (int i = 2; i <= Math.sqrt(x); i++) {       //어떤수의 약수는 그 수의 제곱근보다 작거나 같은 수에서 나온 애들만 찾으면 나머지를 찾을 수 있습니다.
+            if (x % i == 0) {           //1을 제외하고 자기 자신을 나눌 수 있느 수가 존재하지 않아야 합니다.
                 return false;
             }
         }
@@ -23,15 +23,15 @@ public class FindPrimes
 
     public static void main(String[] args)
     {
-        int currentNumber = 2;
+        int currentNumber = 2;          //1을 제외하고 시작해야 합니다.
         int primesCount = 0;
 
         while (primesCount < 50) {
             if (isPrime(currentNumber)) {
-                primesCount++;
+                primesCount++;          //소수의 수를 하나씩 증가시킵니다.
 
                 System.out.print (currentNumber + " ");
-                if (primesCount % 10 == 0) {
+                if (primesCount % 10 == 0) {            //10개가 입력되면 한 칸 띄우기 출력하도록 구현합니다.
                     System.out.println();
                 }
             }
--- 첫번째레포트/src/IrregularArray.java
+++ 첫번째레포트/src/IrregularArray.java
@@ -2,7 +2,7 @@
  * 파일명 : IrregularArray.java
  * 입력 : -
  * 출력 : 행의 개수와 열의 개수 및 데이터에 맞게 값 출력
- * 작성일 : 2020년 4월 일
+ * 작성일 : 2020년 4월 7일
  * 작성자 : 20176088 김경훈
  *
  * Q. 행의 개수와 열의 개수 및 데이터를 입력하여 불규칙한 배열의 결과를 출력
@@ -16,7 +16,7 @@ public class IrregularArray {
         Scanner scanner = new Scanner(System.in);
         System.out.println("배열의 정보를 입력하시오.");
         System.out.print("행의 개수 > ");
-        int row = scanner.nextInt();        //행을 row변수에 넣습니다
+        int row = scanner.nextInt();        //행을 row 변수에 넣습니다
         scanner.nextLine();         //enter에 대해서도 처리해줍니다.
         int intArray[][] = new int[row][];          //행을 초기화한 intarray 배열을 만듭니다.
         System.out.println("열의 개수와 데이터를 입력하시오>");
@@ -24,25 +24,24 @@ public class IrregularArray {
         while (rCount < row) {          //rCount을 이용하여 변하는 행의 개수에 맞춰서 while문 구상합니다.
             String readline = scanner.nextLine();           //하나의 열 전체를 readline 문자열 변수에 넣습니다.
             String[] newline = readline.split(" ", 2);          //띄어쓰기를 기준으로 나누어서 newline에 넣습니다.
-            intArray[rCount] = new int[Integer.parseInt(newline[0])];
+            intArray[rCount] = new int[Integer.parseInt(newline[0])];       //첫 번째 정수가 열 개수이기에 이에 따르게 배열을 만듭니다.
             int index = 0;
-            for (String elem : newline[1].split(" ")){
+            for (String elem : newline[1].split(" ")){          //열을 for을 이용하여 입력하도록 구현합니다.
                 int intElem = Integer.parseInt(elem);
-                intArray[rCount][index++] = intElem;
+                intArray[rCount][index++] = intElem;            //2차원 배열에 값을 입력하도록 합니다.
             }
             rCount++;
         }
         System.out.println("불규칙한 배열의 출력 결과");
         int index2 =0;
-        while(index2 < row){
+        while(index2 < row){        //index2을 이용하여 행을 출력하도록 조건문 작성합니다.
 
             for(int tmp : intArray[index2]){
-                System.out.print(tmp+" ");
+                System.out.print(tmp+" ");          //열을 출력하는 for-each문을 만듭니다.
             }
             System.out.println();
             index2++;
         }
-
     }
 }
 
