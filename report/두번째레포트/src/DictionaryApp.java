91197833b5646ebe0b13d31a463ae49c1393e0b0
kimkyunghun3 <kimkyunghun3@naver.com>
2020-04-19 19:22:20 +0900

    interface 이용하여 Shapes 클래스 구현

--- report/두번째레포트/src/DictionaryApp.java
+++ report/두번째레포트/src/DictionaryApp.java
@@ -1,3 +1,13 @@
+/*
+ * 파일명 : DictionaryApp.java
+ * 입력 : -
+ * 출력 : 키와 값을 하나의 아이템으로 하여 원하는 값을 출력
+ * 작성일 : 2020년 4월 19일
+ * 작성자 : 20176088 김경훈
+ *
+ * Q. PairMap을 상속받는 Dictionary 클래스를 구현하고, 이를 다음과 같이 활용하는 main() 메소드를 가진 DictionaryApp도 작성하라
+ */
+
 class PairMap {
     protected String keyArray [];// key 저장하는 배열
     protected String valueArray []; // value 저장하는 배열
---
+++ report/두번째레포트/src/Shapes.java
@@ -0,0 +1,42 @@
+/*
+ * 파일명 : Shapes.java
+ * 입력 : -
+ * 출력 : 반지름과 면적 나오도록 출력
+ * 작성일 : 2020년 4월 19일
+ * 작성자 : 20176088 김경훈
+ *
+ * Q. 인터페이스 Shape을 구현한 클래스 Circle을 작성하라
+ */
+
+interface Shape {           //Shape 인터페이스 구현
+    final double PI = 3.14;
+    void draw();
+    double getArea();
+    default public void redraw() {      //redraw할 수 있도록 구현
+        draw();
+    }
+}
+
+class Circle implements Shape {
+    private int radius;
+
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    public void draw() {        //draw 구현
+        System.out.print("다시 그립니다. "+"반지름이 " + radius + "인 원입니다.\n");
+    }
+
+    public double getArea() {
+        return radius*radius*PI;
+    }
+}
+
+public class Shapes {
+    public static void main(String[] args) {
+        Shape donut = new Circle(10);       //객체 생성
+        donut.redraw();     //다시 draw하도록 실행
+        System.out.println("면적은 " + donut.getArea());
+    }
+}
