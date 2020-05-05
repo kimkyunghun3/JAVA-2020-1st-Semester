003a9ae10487226e6b4fef78fbd9cacd1c458499
kimkyunghun3 <kimkyunghun3@naver.com>
2020-05-05 14:16:48 +0900

    Token 이용하여 문자끊기

--- 실습/6주차과제/src/StringTockennizer.java
+++ 실습/6주차과제/src/StringTockennizer.java
@@ -1,25 +1,26 @@
+import java.util.StringTokenizer;
 import java.util.Scanner;
 
 public class StringTockennizer {
 
     public static void main(String[] args) {
         System.out.print(">> ");
-        Scanner scanner2 = new Scanner(System.in);
-        String str2 = scanner2.nextLine();
+        Scanner scanner1 = new Scanner(System.in);
+        String str1 = scanner1.nextLine();
 
-        while (!str2.equals("exit")) {
-            String s[] = str2.split(" ");
-            System.out.println("단어 개수는 " + s.length);
+        while (!str1.equals("exit")) { //"exit"를 입력할 때까지 무한 반복
+            StringTokenizer st = new StringTokenizer(str1, " ");
 
-            for (int i = 0; i < s.length; i++) {
-                System.out.println(s[i]);
-            }
+            System.out.println("단어 개수는 " + st.countTokens()); //countTokens(): 토큰의 갯수 새기
+            while (st.hasMoreTokens())
+                System.out.println(st.nextToken()); //nextToken(): 다음 토큰의 내용 반환 즉, 문자열을 단어로 분리
+
             System.out.print(">> ");
-            scanner2 = new Scanner(System.in);
-            str2 = scanner2.nextLine();
+            scanner1 = new Scanner(System.in); //입력 받기
+            str1 = scanner1.nextLine(); //입력을 문자열로 옮기기
         }
-
         System.out.println("종료합니다.");
+
     }
 
 }
