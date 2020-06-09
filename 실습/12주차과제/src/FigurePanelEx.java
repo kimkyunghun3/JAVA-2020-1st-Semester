a8a29c826307c24d285315a9c1792c0482845d7d
kimkyunghun3 <kimkyunghun3@naver.com>
2020-06-09 18:21:30 +0900

    panel 구분하여 각 도형 add하여 grid에 맞게 출력

--- 실습/12주차과제/src/FigurePanelEx.java
+++ 실습/12주차과제/src/FigurePanelEx.java
@@ -3,19 +3,21 @@ import java.awt.*;
 
 
 public class FigurePanelEx extends JFrame {
-    private MP panel = new MP();
-
     public FigurePanelEx() {
         setTitle("FigurePanelEx");
         setLayout(new GridLayout(2, 3, 5, 5));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setContentPane(panel);
         setSize(400, 200);
+        this.add(new xPanel());
+        this.add(new blueRectanglePanel());
+        this.add(new redRoundRectanglePanel());
+        this.add(new blackOvalPanel());
+        this.add(new blueFilledRectanglePanel());
+        this.add(new redFilledRoundRectanglePanel());
         setVisible(true);
-
     }
 
-    class MP extends JPanel {
+    class xPanel extends JPanel {
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
             int width = getSize().width;
@@ -24,18 +26,63 @@ public class FigurePanelEx extends JFrame {
             g.setColor(Color.BLACK);
             g.drawLine(10, 10, width - 10, height - 10);
             g.drawLine(width - 10, 10, 10, height - 10);
+        }
+    }
+
+    class blueRectanglePanel extends JPanel {
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            int width = getSize().width;
+            int height = getSize().height;
+
             g.setColor(Color.BLUE);
             g.drawRect((int) (0.1 * width), (int) (0.1 * height), (int) (0.8 * width), (int) (0.8 * height));
+        }
+    }
+
+    class redRoundRectanglePanel extends JPanel {
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            int width = getSize().width;
+            int height = getSize().height;
+
             g.setColor(Color.RED);
-            g.drawRoundRect(20, 20, 120, 80, 40, 60);
+            g.drawRoundRect((int)(0.1*width), (int)(0.1*height), (int) (0.8 * width), (int) (0.8 * height), 20, 30);
+        }
+    }
+
+    class blackOvalPanel extends JPanel {
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            int width = getSize().width;
+            int height = getSize().height;
+
             g.setColor(Color.BLACK);
             g.drawOval((int) (0.1 * width), (int) (0.1 * height), (int) (0.8 * width), (int) (0.8 * height));
+        }
+    }
+
+    class blueFilledRectanglePanel extends JPanel {
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            int width = getSize().width;
+            int height = getSize().height;
+
             g.setColor(Color.BLUE);
             g.fillRect((int) (0.1 * width), (int) (0.1 * height), (int) (0.8 * width), (int) (0.8 * height));
             g.drawRect((int) (0.1 * width), (int) (0.1 * height), (int) (0.8 * width), (int) (0.8 * height));
+        }
+    }
+
+    class redFilledRoundRectanglePanel extends JPanel {
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            int width = getSize().width;
+            int height = getSize().height;
+
             g.setColor(Color.RED);
-            g.fillRoundRect(20, 20, 120, 80, 20, 40);
-            g.drawRoundRect(20, 20, 120, 80, 20, 40);
+            g.fillRoundRect((int)(0.1*width), (int)(0.1*height), (int)(0.8 * width), (int)(0.8 * height), 20,30);
+            g.drawRoundRect((int)(0.1*width), (int)(0.1*height), (int)(0.8 * width), (int)(0.8 * height), 20,30);
         }
     }
 
